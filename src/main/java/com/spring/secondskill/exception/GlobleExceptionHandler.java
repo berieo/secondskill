4e9ce3a5e215c3f4978bcfe38675ab503579b6ed
Andthen <34112784+berieo@users.noreply.github.com>
2020-12-22 10:42:43 +0800

    GlobelExceptionHandler

--- src/main/java/com/spring/secondskill/exception/GlobleExceptionHandler.java
+++ src/main/java/com/spring/secondskill/exception/GlobleExceptionHandler.java
@@ -1,4 +1,36 @@
 package com.spring.secondskill.exception;
 
+import com.spring.secondskill.result.CodeMsg;
+import com.spring.secondskill.result.Result;
+import com.sun.org.apache.bcel.internal.classfile.Code;
+
+import org.springframework.validation.BindException;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+
+@ControllerAdvice
+@ResponseBody
 public class GlobleExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    public Result<String> exceptionHandler(HttpServletRequest httpServletRequest, Exception e){
+        if(e instanceof BindException){
+            /*
+                exception有多个
+             */
+            BindException ex = (BindException) e;
+            List<ObjectError> erorrs = ex.getAllErrors();
+            ObjectError error = erorrs.get(0);
+            String msg = error.getDefaultMessage();
+            return Result.error(CodeMsg.BIND_ERROR.fillArgs(msg));
+        }else{
+            return Result.error(CodeMsg.SERVER_ERROR);
+        }
+    }
 }
